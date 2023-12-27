37607b15380d55b1fad6f2c7b169056a32265c6d
KingVik-Planet <chukwukingsley56@gmail.com>
2023-12-27 13:35:14 +0100

    Date adding on Pdf

--- main.py
+++ main.py
@@ -1,17 +1,44 @@
+#importing all modules
 import pandas as pd
 import glob
 from fpdf import FPDF
 from pathlib import Path
+
 filepaths = glob.glob("invoices/*xlsx")
 
 for filepath in filepaths:
-    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    invoice_nr = filename.split("-")[0]
+    invoice_nr, date = filename.split("-")
+    Date = filename.split("-")[0]
+
+
     pdf.set_font(family="Times", size=18, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}")
+    pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln = 1)
+
+
+    pdf.set_font(family="Times", size=18, style="B")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}", ln = 1)
+
+    df = pd.read_excel(filepath, sheet_name="Sheet 1")
+    for index, row in df.iterrows():
+        pdf.set_font(family="Times", size=18)
+        pdf.set_text_color(r=100, g=100, b=100)
+        pdf.cell(w=30, h=8, txt=str(row["product_id"]), border=1)
+        pdf.cell(w=70, h=8, txt=str(row["product_name"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["amount_purchased"]), border= 1)
+        pdf.cell(w=30, h=8, txt=str(row["price_per_unit"]), border=1)
+        pdf.cell(w=30, h=8, txt=str(row["total_price"]), border= 1, ln =1)
+
+
+
+    # Outputing the files
     pdf.output(f"Outputs/{filename}.pdf")
+
+
 
     #print(df)
